bf4ed06b64e921bc850085d8f9b809b11f5247ce
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-10-26 10:57:42 +0600

    avatarGlow

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,8 +6,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: Center(
+        child: AvatarGlow(
+          duration: const Duration(milliseconds: 500),
+          glowRadiusFactor: 0.9,
+          glowCount: 2,
+          glowColor: Colors.deepPurple,
+          child: const Icon(Icons.favorite,size: 44,),
+        ),
+      ),
     );
   }
 }
